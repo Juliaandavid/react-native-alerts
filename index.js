c5e142ea9daff54ae6d391c117d352401f6b24f0
Julian Izquierdo <julian.izquierdo@mocionsoft.com>
2016-12-26 08:59:25 -0500

    Configurando confirm

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ import { NativeModules } from 'react-native';
 class Alerts {
 
   /**
-   * Allows show native alerts
+   * Allows to show native alert
+   * Props: {
+   *    title (optional)
+   *    message (optional)
+   *    button (optional)
+   * }
    */
   static alert (options, cb) {
       NativeModules.RNAlerts.alert(options, cb);
   }
 
+  /**
+   * Allows to show native alert confirm
+   * Props: {
+   *    title (optional)
+   *    message (optional)
+   *    buttonAccept (optional)
+   *    buttonCancel (optional)
+   * }
+   */
   static confirm (options, cb) {
-	  //NativeModules.RNAlerts.confirm(options, cb);
+	  NativeModules.RNAlerts.confirm(options, cb);
   }
 
   static prompt (options, cb) {
